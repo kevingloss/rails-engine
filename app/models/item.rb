18f5c0c3b26608d584d6325ddd0e712423bab7ea
Kevin Gloss <kgloss89@gmail.com>
2022-02-08 14:48:57 -0700

    Remove merchant id attribute

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :merchant
-  validates_presence_of :name, :description, :unit_price, :merchant_id
+  validates_presence_of :name, :description, :unit_price
   validates_numericality_of :unit_price, greater_than: 0
   validates_numericality_of :merchant_id, greater_than: 0, integer_only: true
 end
