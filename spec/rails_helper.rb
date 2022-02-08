11116a3ce5c9e1263952da2eb9a5443192eb8123
Kevin Gloss <kgloss89@gmail.com>
2022-02-08 11:35:58 -0700

    Update rails helper to load support spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,9 +33,15 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # Include the spec/support module when testing requests
+  config.include RequestSpecHelper, type: :request
 
   # Add this line to utilize factorybot
   config.include FactoryBot::Syntax::Methods
