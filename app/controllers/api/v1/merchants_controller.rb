f5370dc391f0cc6a7feb612cbe1423f425b752c6
Kevin Gloss <kgloss89@gmail.com>
2022-02-08 14:49:48 -0700

    Update merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,5 @@
-class Api::V1::ItemsController < ApplicationController 
+class Api::V1::MerchantsController < ApplicationController 
   def index 
-    
+    json_response(MerchantSerializer.new(Merchant.all))
   end
 end
