ab3cc8240e42f650ccba6df0657713cedfac01cd
Kevin Gloss <kgloss89@gmail.com>
2022-02-09 19:16:05 -0700

    Test merchant search method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Merchant do
   describe 'relationships' do
     it { should have_many(:items) }
   end
+
+  describe 'class methods' do 
+    it 'searches for merchants by name returns first alphabetically' do 
+      create_list(:merchant, 100)
+      create(:merchant, name: 'aaa')
+      merchant = create(:merchant, name: 'AAA')
+
+      expect(Merchant.search('aaa')).to eq(merchant)
+    end
+  end
 end
