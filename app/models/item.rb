51072a2b3cb5fd7622c923f61d0f7eba67fabe27
Kevin Gloss <kgloss89@gmail.com>
2022-02-10 12:51:09 -0700

    Feat class methods for searching

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,18 @@ class Item < ApplicationRecord
   validates_presence_of :name, :description, :unit_price
   validates_numericality_of :unit_price, greater_than: 0
   validates_numericality_of :merchant_id, greater_than: 0, integer_only: true
+
+  def self.search_all_names(name)
+    where("name ILIKE ?", "%#{name}%").order(:name)
+  end
+
+  def self.search_all_by_price(min_price: nil, max_price: nil)
+    if min_price && max_price
+      where(:unit_price => min_price..max_price).order(:unit_price)
+    elsif min_price
+      where("unit_price >= ?", min_price).order(:unit_price)
+    else
+      where('unit_price <= ?', max_price).order(unit_price: :desc)
+    end
+  end
 end
