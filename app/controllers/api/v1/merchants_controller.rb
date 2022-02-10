6f0fb4dfc5de23ffeab504d00e8ac41e49c2acd6
Kevin Gloss <kgloss89@gmail.com>
2022-02-09 19:15:25 -0700

    Feat find merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -9,6 +9,16 @@ class Api::V1::MerchantsController < ApplicationController
     json_response(MerchantSerializer.new(@merchant))
   end
 
+  def find
+    if params[:name] && params[:name] != ''
+      merchant = Merchant.search(params[:name])
+      return json_response({data: {}}) if !merchant
+      json_response(MerchantSerializer.new(merchant))
+    else
+      render json: {error: 'Name param must be valid string.'}, status: :bad_request
+    end
+  end
+
   private
     def set_merchant
       @merchant = Merchant.find(params[:id])
