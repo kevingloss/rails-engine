2180c8c14b6c62bda8031683e9f7a8806ebdc40a
Kevin Gloss <kgloss89@gmail.com>
2022-02-09 19:16:22 -0700

    Add merchant search feat

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ApplicationRecord 
   has_many :items 
   validates_presence_of :name
+
+  def self.search(name)
+    where("name ILIKE ?", "%#{name}%").order(:name).limit(1).first
+  end
 end
